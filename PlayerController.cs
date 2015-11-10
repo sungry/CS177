a0057bf87a890fd7e67ca81f2accf6342b14bc9a
EricSFChu <echu6@mail.ccsf.edu>
2015-11-10 13:59:37 -0800

    Here are some sample scripts I'm using The Camera Controller is for a 3D environment and follows your character in a third person perspective The PlayerController allows a person to control the character through the keyboard SomeSortOfBehavior changes the color of objects while listening for keystrokes Rotations rotate objects along x, y , z axes

---
+++ PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody rb;
+    private int count;
+    public float speed;
+    public Text countText;
+    public Text winText;
+
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        setCountText();
+        winText.text = "";
+    }
+
+    void FixedUpdate () //physics operations
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement * speed);
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            setCountText();
+        }
+    }
+
+    void setCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 10)
+            winText.text = "You Win!";
+    }
+}
