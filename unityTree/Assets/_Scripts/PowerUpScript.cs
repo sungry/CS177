5fc6b47645435baa108a902e62a031a00c1a2f35
EricSFChu <echu6@mail.ccsf.edu>
2015-11-24 14:25:06 -0800

    Added an additional collider box to powerups so that physics as well as trigger can be implemented

--- unityTree/Assets/_Scripts/PowerUpScript.cs
+++ unityTree/Assets/_Scripts/PowerUpScript.cs
@@ -6,12 +6,13 @@ public class PowerUpScript : MonoBehaviour {
     HUDScript hud;
 
     void OnTriggerEnter2D(Collider2D other)
+    //void OnCollissionExit2D(Collider2D other)
     {
         if(other.tag =="Player")
         {
             hud = GameObject.Find("Main Camera").GetComponent<HUDScript>();
             hud.IncreaseScore(100);
-            Destroy(this.gameObject);
+            //Destroy(this.gameObject);
         }
     }
 }
