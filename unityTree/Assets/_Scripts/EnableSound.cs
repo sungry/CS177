0b0092006a04f918f1965c74525b5688734e13cc
kokixi <anagrande@gmail.com>
2015-11-25 15:49:51 -0800

    add settingsScreen

---
+++ unityTree/Assets/_Scripts/EnableSound.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class EnableSound : MonoBehaviour {
+
+    public Toggle soundToggle;
+
+    void Start() {
+
+        soundToggle = soundToggle.GetComponent<Toggle> ();
+    }
+
+    public void SetMode(){
+        if (soundToggle.isOn) {
+            GameSystem.soundEnabled = true;
+        }
+        else {
+            GameSystem.soundEnabled = false;
+        }
+
+		Debug.Log (GameSystem.soundEnabled);
+    }
+}
--- unityTree/Assets/_Scripts/mainMenu.cs
+++ unityTree/Assets/_Scripts/mainMenu.cs
@@ -2,22 +2,23 @@
 using UnityEngine.UI;
 using System.Collections;
 
-public class mainMenu : MonoBehaviour {
+public class MainMenu : MonoBehaviour {
 
-	public Button startButton;
-	public Button options;
+    public Button startButton;
+    public Button options;
 
-	// Use this for initialization
-	void Start () {
-		startButton = startButton.GetComponent<Button> ();
-		options = options.GetComponent<Button> ();
-	}
-	
-	public void StartLevel() {
-		Application.LoadLevel (1);
-	}
+// Use this for initialization
+    void Start () {
+		Debug.Log ("en menu" + GameSystem.soundEnabled);
+        startButton = startButton.GetComponent<Button> ();
+        options = options.GetComponent<Button> ();
+    }
 
-	public void StartOptions() {
-		Application.LoadLevel (3);
-	}
+    public void StartLevel() {
+        Application.LoadLevel (1);
+    }
+
+    public void StartOptions() {
+        Application.LoadLevel (3);
+    }
 }
