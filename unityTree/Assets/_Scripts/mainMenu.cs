b0e718349f51f8fc8c4ce6b11a357d5d7240b072
kokixi <anagrande@gmail.com>
2015-11-24 20:41:03 -0800

    add menu screen

---
+++ unityTree/Assets/_Scripts/mainMenu.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class mainMenu : MonoBehaviour {
+
+	public Button startButton;
+	public Button options;
+
+	// Use this for initialization
+	void Start () {
+		startButton = startButton.GetComponent<Button> ();
+		options = options.GetComponent<Button> ();
+	}
+	
+	public void StartLevel() {
+		Application.LoadLevel (1);
+	}
+
+	public void StartOptions() {
+		Application.LoadLevel (3);
+	}
+}
