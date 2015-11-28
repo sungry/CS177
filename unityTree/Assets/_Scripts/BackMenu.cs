9683d309ccdcd26e331e8686bc4038df481f62c9
wildelf <psizzlepip@yahoo.com>
2015-11-27 19:54:09 -0800

    Revert "Revert "Menu screen and settings screen""
    This reverts commit 25319870a6c3df3b1b741dfc81fe3e6e64bd5b47.
    
    Development does not need or want this revert

---
+++ unityTree/Assets/_Scripts/BackMenu.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class BackMenu : MonoBehaviour {
+
+	public Button goBackButton;
+
+	void Start() {
+		goBackButton = goBackButton.GetComponent<Button> ();
+	}
+
+	public void Back() {
+		Application.LoadLevel (0);
+	}
+
+}
