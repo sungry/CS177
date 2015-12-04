aa49b98d618926b747b2d2e9a25b0366cb8c8b33
wildelf <psizzlepip@yahoo.com>
2015-12-03 17:28:29 -0800

    music updated for all scenes; new universal background music controller script; deleted all prototype music files

--- unityTree/Assets/_Scripts/GameOverScript.cs
+++ unityTree/Assets/_Scripts/GameOverScript.cs
@@ -9,17 +9,14 @@ public class GameOverScript : MonoBehaviour {
 	void Start ()
     {
         score = PlayerPrefs.GetInt("Score");
-        if (GameSystem.soundEnabled == false) {
-            audioSource = audioSource.GetComponent<AudioSource>();
-            audioSource.mute = true;
-        }
     }
 
     void OnGUI()
     {
         GUI.Label(new Rect(Screen.width / 2 - 40, 50, 80, 30), "GAME OVER");
         GUI.Label(new Rect(Screen.width / 2 - 40, 300, 80, 30), "Score: " + score);
-           if(GUI.Button (new Rect(Screen.width / 2-30, 350, 60, 30), "Retry?"))
+        
+		if(GUI.Button (new Rect(Screen.width / 2-30, 350, 60, 30), "Retry?"))
         {
             Application.LoadLevel(1);
         }
