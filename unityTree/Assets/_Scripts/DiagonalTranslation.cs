49821f1c0c4f12b4dac075e93a685df95533b8c3
EricSFChu <echu6@mail.ccsf.edu>
2015-12-08 22:17:38 -0800

    scripts

---
+++ unityTree/Assets/_Scripts/DiagonalTranslation.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DiagonalTranslation : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        transform.Translate((-1.2f) * Time.deltaTime, (-1.2f) * Time.deltaTime, 0, Camera.main.transform);
+    }
+}
